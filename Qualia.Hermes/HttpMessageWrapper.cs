21dd5154cbb651bf361b6709f7fae97dd9e6922a
PagonisTasos <apagon@protonmail.com>
2022-02-02 22:22:15 +0200

    Add project files.

---
+++ Qualia.Hermes/HttpMessageWrapper.cs
@@ -0,0 +1,17 @@
+﻿namespace Qualia.Hermes
+{
+    public abstract class HttpMessageWrapper<TInner, TOutter> 
+        where TInner : class, new()
+        where TOutter : HttpMessageWrapper<TInner, TOutter>, new()
+    {
+        public TInner Message { get; private set; }
+        protected HttpMessageWrapper()
+        { }
+        public static TOutter From(TInner msg)
+        {
+            var outter = new TOutter();
+            outter.Message = msg;
+            return outter;
+        }
+    }
+}
